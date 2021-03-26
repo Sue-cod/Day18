d279118bf246a14f0f180f567b0d7ae997e0031e
Sue-cod <snozic892@gmail.com>
2021-03-26 01:12:46 +0100

    peti zadatak

---
+++ ex05/src/index.js
@@ -0,0 +1,24 @@
+let shoppingList = new Map([
+['Banana', 3],
+['Pineapple', 5],
+['Pear', 2],
+['Carrot', 10],
+['Apple', 1.5],
+]);
+
+var arrKey = [];
+var arrValue = [];
+
+for (let product of shoppingList.entries()) {
+    arrKey.push(`groceries: ${product[0]}`);
+    arrValue.push(`amount: ${product[1]}`);
+}
+
+console.log(arrKey);
+console.log(arrValue);
+
+for (let i of shoppingList) {
+    console.log(i);
+}
+
+module.exports = { shoppingList, arrKey, arrValue };
